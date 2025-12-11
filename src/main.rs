18ed5aec9f958288a3eafaca9289db8d8145b457
Dawaman43 <dawitworkujima@gmail.com>
2025-12-11 12:18:24 +0300

    implement first_word function and update main to demonstrate its usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let my_s = String::from("Hello world");
+  let f_s =   first_word(&my_s);
+  println!("The first word is: {}", f_s);
+
+}
+
+fn first_word(s: &str)-> &str{
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return &s[0..i];
+        }
+        
+    }
+    &s[..]
 }
